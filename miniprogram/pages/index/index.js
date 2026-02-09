cc93b4ca3b9cde34345e28f006a9e2a4924bbbf8
RampageLi <384650157@qq.com>
2026-02-09 16:40:29 +0800

    feat: change pic order

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -71,8 +71,6 @@ Page({
 
             // 轮播图1
             swiper1: [
-                'https://pic1.imgdb.cn/item/6989979f01b1be04704efea7.jpg',
-                'https://pic1.imgdb.cn/item/6989979f01b1be04704efea6.jpg',
                 'https://pic1.imgdb.cn/item/698997c101b1be04704efeb6.jpg',
                 'https://pic1.imgdb.cn/item/698997c101b1be04704efeb7.jpg',
                 'https://pic1.imgdb.cn/item/698997c101b1be04704efeb5.jpg',
@@ -80,6 +78,8 @@ Page({
                 'https://pic1.imgdb.cn/item/698997a001b1be04704efeaa.jpg',
                 'https://pic1.imgdb.cn/item/698997a001b1be04704efea9.jpg',
                 'https://pic1.imgdb.cn/item/698997a001b1be04704efea8.jpg',
+                'https://pic1.imgdb.cn/item/6989979f01b1be04704efea7.jpg',
+                'https://pic1.imgdb.cn/item/6989979f01b1be04704efea6.jpg',
             ],
 
             // 连续图
